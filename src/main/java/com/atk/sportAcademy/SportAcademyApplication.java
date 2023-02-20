362e3aef01bd2b54e98404f7418a3b0c90aa695c
eminkabacaoglu <eminkabacaoglu@gmail.com>
2023-02-20 15:08:22 +0300

    project

---
+++ src/main/java/com/atk/sportAcademy/SportAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.atk.sportAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atk/sportAcademy/SportAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atk.sportAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
